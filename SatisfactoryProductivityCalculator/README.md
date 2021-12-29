74559ef042ce007c98d8a6f53976e5079df74fb3
SurveyorCangMing <82685282+SurveyorCangMing@users.noreply.github.com>
2021-12-29 23:08:01 +0800

    build 5

--- SatisfactoryProductivityCalculator/README.md
+++ SatisfactoryProductivityCalculator/README.md
@@ -1,29 +1,3 @@
 # Satisfactory Productivity Calculator
 
-version: 2.0 (build 3)
-
-## usage
-
-### type list
-
-#### 1 electricity
-
-1 
-
-#### 2 miner
-
-1 Fe
-
-2 Cu
-
-3 
-
-4 Caterium
-
-#### 3 conveyor
-
-#### 4 produce
-
-1 
-
-## Development and Testing
+version: t-2.0.1 (build 5)
--- SatisfactoryProductivityCalculator/SatisfactoryProductivityCalculator.cpp
+++ SatisfactoryProductivityCalculator/SatisfactoryProductivityCalculator.cpp
@@ -2,15 +2,15 @@
 #include<Windows.h>
 using namespace std;
 int sum, n, t;
-struct Buildings {
+struct Building {
 	int type = -1;
-	int level = -1;//
+	int level = -1;
 	int custom = -1;
 	int clockspeed = -1;
 	int input = -1;
 	int output = -1;
-	int line = -1;//
-}building[501];
+	int line = -1;
+}buildings[501];
 struct miner {
 	//mine
 	int type = -1;
@@ -31,50 +31,57 @@ struct process {
 }processers[121];
 void show_list() {
 	for (int i = 1;i <= n;i++) {
-		if (building[i].type != 0) {
-			cout << building[i].type << " " << building[i].level << " " << building[i].custom << " " << building[i].clockspeed;
+		if (buildings[i].type != 0) {
+			cout << buildings[i].type << " " << buildings[i].level << " " << buildings[i].custom << " " << buildings[i].clockspeed;
 		}
 	}
 }
-void add() {
-	int t,type,clockspeed;
+void add(int n) {
+	int t;
 	cout << "type:\n[1] power\n[2] miner\n[3] conveyor\n[4] production\n";
 	cin >> t;
 	switch (t) {
 		case 1: //power
-			cout << "type list:\n[1]树叶[2]花瓣\n[3]木头\n[4]菌丝体\n[5]外星甲壳\n[6]外星器官\n[7]生物质\n[8]固态生物燃料\n";
+			cout << "type list:\n[1]树叶\n[2]花瓣\n[3]木头\n[4]菌丝体\n[5]外星甲壳\n[6]外星器官\n[7]生物质\n[8]固态生物燃料\n";
 			cout << "[9]煤炭\n[10]燃油\n[11]地热能\n[12]核能\n";
-			cin >> type;
+			cin >> buildings[n].type;
 			break;
 		case 2: //miner
-			cout << "type list:\n[1]铁\n[2]铜\n[3]石灰石\n[4]硫\n[5]钦\n[6]原石英\n[7]炭\n[8]S.A.M\n[9]铝土\n[10]铀";
-			cin >> type;
-			type += 12;
+			cout << "type list:\n[1]铁\n[2]铜\n[3]石灰石\n[4]硫\n[5]钦\n[6]原石英\n[7]炭\n[8]S.A.M\n[9]铝土\n[10]铀\n";
+			cin >> buildings[n].type;
+			buildings[n].type += 12;
 			break;
 		case 3: //conveyor
-			type = 23;
+			buildings[n].type = 23;
 			break;
 		case 4: //production
-
+			cout << "type list:\n";
+			//smelter
+			cout << "Smelter\n[1]铁\n[2]铜\n[3]钦\n[4]铝锭(替代配方)\n";
+			//Constructor
+			cout << "Constructor\n[5]铁板\n[6]铁棒\n[7]螺丝\n[8]铁锭制螺丝(替代配方)\n[9]电线\n[10]电缆\n[11]铁锭制电线(替代配方)\n";
+			buildings[n].type += 23;
 			break;
 		default:
 			cout << "wrong type.\n";
 			return;
 			break;
 		}
-	cin >> clockspeed;
+	cin >> buildings[n].clockspeed;
 }
 int main() {
+	int num = 1;
 	char operation;
 	while (true) {
 		cout << "[+] add building\n[-] delete building\n[=] print productivity\n[s] save\n";
 		cin >> operation;
 		switch (operation) {
 			case '+':
-				add();
+				add(num);
+				num++;
 			case '-':
 				cin >> t;
-				building[t].type = 0;
+				buildings[t].type = -1;
 				break;
 			case '=':
 				show_list();
@@ -97,6 +104,7 @@ int main() {
 				cout << "wrong command\n";
 				break;
 		}
+		system("cls");
 	}
 	return 0;
 }
